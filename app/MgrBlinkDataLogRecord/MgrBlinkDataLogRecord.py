bfe255d9e7c35168424005a1dcb2debdeeb9ef44
ba-haile <haivsgd@gmail.com>
2019-04-24 16:20:03 +0200

    Update blink packet log recording in manager

--- app/MgrBlinkDataLogRecord/MgrBlinkDataLogRecord.py
+++ app/MgrBlinkDataLogRecord/MgrBlinkDataLogRecord.py
@@ -36,7 +36,8 @@ def handle_data(notifName, notifParams):
             for neighbor_id, rssi in neighbors:
                 print '    --> Neighbor ID = {0},  RSSI = {1}'.format(neighbor_id, rssi)
                 listRSSI.append(rssi)
-                listNeighbor.append(neighbor_id)                
+                listNeighbor.append(neighbor_id) 
+                           
             if len(listNeighbor) == 1:
                 jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])}]}
             elif len(listNeighbor) == 2:
@@ -45,12 +46,17 @@ def handle_data(notifName, notifParams):
                 jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])},{'MoteID': '{0}'.format(listNeighbor[1]), 'RSSI': '{0}'.format(listRSSI[1])},{'MoteID': '{0}'.format(listNeighbor[2]), 'RSSI': '{0}'.format(listRSSI[2])}]}
             else:
                 jsonRssi = {'Time': '{}'.format(datetime.datetime.now()), 'Payload':'{}'.format(data),'BlinkAddress': '{0}'.format(mac_address), 'HeardMote': [{'MoteID': '{0}'.format(listNeighbor[0]), 'RSSI': '{0}'.format(listRSSI[0])},{'MoteID': '{0}'.format(listNeighbor[1]), 'RSSI': '{0}'.format(listRSSI[1])},{'MoteID': '{0}'.format(listNeighbor[2]), 'RSSI': '{0}'.format(listRSSI[2])},{'MoteID': '{0}'.format(listNeighbor[3]), 'RSSI': '{0}'.format(listRSSI[3])}]}
+            '''
             with open('RSSIvalue.json', 'a') as f:                    
-                f.write(json.dumps(jsonRssi))
-                f.write('\n')                                                    
+                f.dump(json.dumps(jsonRssi))            
+                f.write('\n')       
+            '''                                  
             if not neighbors:
                 print '    --> Neighbors = n/a'            
             print '    --> Data Sent = {0}\n\n'.format(data.encode("hex"))
+            with open('manager.json', 'a') as f:                    
+                f.dump(json.dumps(jsonRssi))            
+                f.write('\n')
             
     except:
         # handle non-blink data
