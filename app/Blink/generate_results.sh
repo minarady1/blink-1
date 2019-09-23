30007041482163f668d4686b270faa9b9851277c
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-09-23 12:26:17 +0200

    Add generate_result.sh

---
+++ app/Blink/generate_results.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "analyzer.py"
+./analyzer.py measurements/log-blink-manager-20190919-143138.jsonl
+
+echo "plot_otti_csv.py"
+./plot_otti_csv.py measurements/blink_1.csv
+
+echo "plot_blink_command_profile.py"
+./plot_blink_command_profile.py measurements/blink-command
