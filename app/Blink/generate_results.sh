fc2358afcbe0bf3db15b94a0c87f731b7a540c18
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-09-24 15:27:06 +0200

    Rename log files

--- app/Blink/generate_results.sh
+++ app/Blink/generate_results.sh
@@ -3,10 +3,10 @@ echo "genereate deployment.png"
 python ./draw.py -o results/deployment.png
 
 echo "analyzer.py"
-./analyzer.py measurements/log-blink-manager-20190919-143138.jsonl
+./analyzer.py measurements/log-blink-manager-0919.jsonl
 
 echo "plot_otti_csv.py"
-./plot_otti_csv.py measurements/blink_1.csv
+./plot_otti_csv.py measurements/otti-blink.csv
 
 echo "plot_blink_command_profile.py"
 ./plot_blink_command_profile.py measurements/blink-command
