5ba4b5d8d25d2406fd3581aa6172ed1288cf0fab
Yasuyuki Tanaka <yasuyuki.tanaka@inria.fr>
2019-09-24 16:02:19 +0200

    s/otti/otii/

--- app/Blink/generate_results.sh
+++ app/Blink/generate_results.sh
@@ -5,8 +5,8 @@ python ./draw.py -o results/deployment.png
 echo "analyzer.py"
 ./analyzer.py measurements/log-blink-manager-0919.jsonl
 
-echo "plot_otti_csv.py"
-./plot_otti_csv.py measurements/otti-blink.csv
+echo "plot_otii_csv.py"
+./plot_otii_csv.py measurements/otti-blink.csv
 
 echo "plot_blink_command_profile.py"
 ./plot_blink_command_profile.py measurements/blink-command
